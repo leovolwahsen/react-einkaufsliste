1acd71c7963e7087147de2062979ea7ccf249df8
Leo <volwahsenl@gmail.com>
2023-10-17 21:26:03 +0200

    burger menue style responsive

--- my-app/src/components/Navigation/Navigation.jsx
+++ my-app/src/components/Navigation/Navigation.jsx
@@ -11,7 +11,7 @@ function Navigation() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: 'fit-content' }}
             navbarScroll
           >
             <Nav.Link href="/">Hauptseite</Nav.Link>
